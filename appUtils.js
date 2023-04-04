898d128706564fb7e01ed3e358291984562eaba2
AbhiSingh0302 <abhimanyusingh0302@gmail.com>
2023-04-04 11:26:47 +0530

    node async pattern 2

--- appUtils.js
+++ appUtils.js
@@ -1,15 +1,15 @@
-const {readFile,writeFile} = require('fs');
-const utils = require('util');
+const {readFile,writeFile} = require('fs').promises;
+// const utils = require('util');
 
-const readFilePromise = utils.promisify(readFile);
-const writeFilePromise = utils.promisify(writeFile);
+// const readFilePromise = utils.promisify(readFile);
+// const writeFilePromise = utils.promisify(writeFile);
 
 const start = async () => {
     try{
-        const first = await readFilePromise('./content/first.txt','utf-8');
-        const second = await readFilePromise('./content/sec.txt','utf-8');
-        await writeFilePromise('./content/resultUtil.txt',
-        `Hello welcome ${first} and ${second} in resultUtil`);
+        const first = await readFile('./content/first.txt','utf-8');
+        const second = await readFile('./content/sec.txt','utf-8');
+        await writeFile('./content/resultUtil.txt',
+        `Hello welcome to promises, ${first} and ${second} in resultUtil`);
         console.log(first,second);
     }catch(err){
         console.log(err);
